ca6b5d53a34cfd465758ffaa8c4bfb064df8bf5d
Drake Damon <dddamon06@gmail.com>
2024-04-09 22:02:20 -0400

    completed array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, searchString) {
+  return drivers.filter(function(driversName) {
+    return driversName.toLowerCase() === searchString.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, testString) {
+return drivers.filter(function(driversName) {
+ return driversName.indexOf(testString) === 0;
+});
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function(driver) {
+    return driver.name === name;
+  });
+}
+
